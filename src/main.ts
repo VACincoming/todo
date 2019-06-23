2943cb3a15897fdeb609de1df80e87ac084b78e0
VACincoming <alexn98@mail.ru>
2019-06-24 00:35:09 +0300

    fix some responsive bugs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
 import ElementUI from "element-ui";
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
